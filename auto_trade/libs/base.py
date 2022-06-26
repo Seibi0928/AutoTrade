239bb2e536de196007ecca094cbf871ebe3763c5
Harunari <ryo13579978@gmail.com>
2022-06-26 12:28:53 +0900

    first commit.

---
+++ auto_trade/libs/base.py
@@ -0,0 +1,11 @@
+"""
+auto_trade base module.
+This is the principal module of the project_name project.
+here you put your main classes and objects.
+Be creative! do whatever you want!
+If you want to replace this with a Flask application run:
+    $ make init
+and then choose `flask` as template.
+"""
+
+NAME = "auto_trade"
---
+++ auto_trade/main.py
@@ -0,0 +1,5 @@
+"""Entry point for auto_trade."""
+from libs.cli import hoge  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    hoge()
